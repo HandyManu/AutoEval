0a3634b5bba00b3a9388aa3a2ea25beb44257ac1
Manuel <20220416@ricaldone.edu.sv>
2025-06-17 21:36:35 -0600

    prueba

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -2,8 +2,19 @@ import express from "express";
 
 import taskRoutes from "../BackEnd/src/routes/taskRotes.js"
 
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+
+// Configuración de Swagger
+//traemos el json
+const swaggerDocumment = JSON.parse(
+    fs.readFileSync(path.resolve("./AutoEv.json"), "utf8")
+);
+
+app.use("/api/AutoEv", swaggerUi.serve, swaggerUi.setup(swaggerDocumment));
 
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
